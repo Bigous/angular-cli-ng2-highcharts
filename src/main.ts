121c94834375cbc6ef42d3ea94b9884edfbecdc9
Richard Natal <bigous@gmail.com>
2016-08-18 17:21:35 -0300

    First usable commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+window['Highcharts'] = require('highcharts');
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule, environment } from './app/';
